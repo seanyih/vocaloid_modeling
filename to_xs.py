1cb95104e0e6e0ff97db3973187cb84697852bd7
WeiFu3024 <willfu666@gmail.com>
2025-02-28 01:03:21 +0800

    Implement pyneuralfx for model training. Add grid search method.

--- to_xs.py
+++ to_xs.py
@@ -2,11 +2,11 @@ import os
 import soundfile as sf
 
 # Source and destination directories.
-src_dir = '/home/fu/Documents/vocaloid_modeling/vocaloid_modeling/vocaloid_modeling/data/audio'
-dst_dir = '/home/fu/Documents/vocaloid_modeling/vocaloid_modeling/vocaloid_modeling/data/audio_25s'
+src_dir = './vocaloid_modeling/vocaloid_modeling/data/audio'
+dst_dir = './vocaloid_modeling/vocaloid_modeling/data/audio_60s'
 os.makedirs(dst_dir, exist_ok=True)
 # Duration in seconds to extract.
-duration = 25
+duration = 60
 
 # List all files in the source directory.
 original_files = os.listdir(src_dir)
@@ -22,12 +22,12 @@ for file in original_files:
     num_samples = duration * samplerate
     
     # Slice the array to get the first 5 seconds.
-    data_25s = data[:num_samples]
+    data_60s = data[:num_samples]
     
     # Create the full destination file path.
     dst_path = os.path.join(dst_dir, file)
     
     # Write the 5-second clip to the destination.
-    sf.write(dst_path, data_25s, samplerate)
+    sf.write(dst_path, data_60s, samplerate)
     
-    print(f'Processed {file}: saved first 25 seconds to {dst_path}')
+    print(f'Processed {file}: saved first 60 seconds to {dst_path}')
